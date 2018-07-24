7557d6a0d3221666d5e8cc55099839e9f84aca8d
mistmr7 <michael.mistarz@gmail.com>
2018-07-24 15:27:21 -0400

    commit new changes and if statement

--- ticketmaster.js
+++ ticketmaster.js
@@ -1,7 +1,7 @@
 var key = "HC4MdAnqSfzwkac8R6UyzqQbTcHqzGuL";
 var secret = "oUttZpkLGpTyWkuf";
 //query url including raleigh location
-var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + key + "&city=Raleigh"
+var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + key + "&city=Raleigh" + "&sort=date,asc"
 console.log("queryurl: " + queryURL);
 
 $.ajax({
@@ -17,6 +17,7 @@ $.ajax({
 function showEvents(json) {
     
     for(var i=0; i<json.page.size; i++) {
+        
         // var tr = $('<tr>')
         // var td = $('<td>')
         // var eventname = tr + td + json._embedded.events[i].name;
@@ -38,12 +39,19 @@ function showEvents(json) {
         var td1 = $('<td>').text(eventName);
         var startDate = json._embedded.events[i].dates.start.localDate;
         var td2 = $('<td>').text(startDate);
-        var min = json._embedded.events[i].priceRanges[0].min;
-        var max = json._embedded.events[i].priceRanges[0].max;
+        let min = json._embedded.events[i].priceRanges[0].min;
+        let max = json._embedded.events[i].priceRanges[0].max;
+        if(!json._embedded.events[i].priceRanges === undefined){
+            return min = ''
+            return max = ''
+        } else {
+        }
+        console.log(min)
         var td3 = $('<td>').text(`$${min} - $${max}`);
         var startTime = json._embedded.events[i].dates.start.localTime;
         var td4 = $('<td>').text(startTime);
-
+        
+        
         row.append(td1).append(td2).append(td3).append(td4);
         $('tbody').append(row);
     };
