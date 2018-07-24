fbff581f1faa9c9deb27dde2aac9090b0c22ded9
mistmr7 <michael.mistarz@gmail.com>
2018-07-24 11:24:17 -0400

    fixed the table issues and got the event to search for band name instead of event name

--- ticketmaster.js
+++ ticketmaster.js
@@ -10,26 +10,41 @@ $.ajax({
     datatype: "json",
 }).then(function(json) {
     console.log(json);
-    console.log(json._embedded.events[0]);
+    console.log(json._embedded.events[0]._embedded.attractions[0].name);
     showEvents(json);
 });
 
 function showEvents(json) {
+    
     for(var i=0; i<json.page.size; i++) {
-        var tr = "<tr>"
-        var td = "<td>"
-        var eventname = tr + td + json._embedded.events[i].name;
-        var startdate = td + json._embedded.events[i].dates.start.localDate;
-        var min = td + "$" +json._embedded.events[i].priceRanges[0].min;
-        var max = "$" + json._embedded.events[i].priceRanges[0].max;
-        var picture = json._embedded.events[i].images[0].url;
-        var img = $("<img>")
-        $(img).attr("src", picture)
-        //ADD CORRECT LOCATION
-        $("tbody").append(eventname);
-        $("tbody").append(startdate);
-        $("tbody").append(min + "-" + max);
-        $("tbody").append(picture);
-        $("tbody").append(img);
-    }
+        // var tr = $('<tr>')
+        // var td = $('<td>')
+        // var eventname = tr + td + json._embedded.events[i].name;
+        // var startdate = td.text(json._embedded.events[i].dates.start.localDate);
+        // var min = td + "$" +json._embedded.events[i].priceRanges[0].min;
+        // var max = "$" + json._embedded.events[i].priceRanges[0].max;
+        // var picture = json._embedded.events[i].images[0].url;
+        // var img = $("<img>")
+        // $(img).attr("src", picture)
+        // //ADD CORRECT LOCATION
+        // $("tbody").append(eventname);
+        // $("tbody").append(startdate);
+        // $("tbody").append(min + "-" + max);
+        // $("tbody").append(picture);
+        // $("tbody").append(img);
+
+        var row = $('<tr>');
+        var eventName = json._embedded.events[i]._embedded.attractions[0].name;
+        var td1 = $('<td>').text(eventName);
+        var startDate = json._embedded.events[i].dates.start.localDate;
+        var td2 = $('<td>').text(startDate);
+        var min = json._embedded.events[i].priceRanges[0].min;
+        var max = json._embedded.events[i].priceRanges[0].max;
+        var td3 = $('<td>').text(`$${min} - $${max}`);
+        var startTime = json._embedded.events[i].dates.start.localTime;
+        var td4 = $('<td>').text(startTime);
+
+        row.append(td1).append(td2).append(td3).append(td4);
+        $('tbody').append(row);
+    };
 };
